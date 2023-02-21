7d5057cf2267175c1513a844347b01422bb088c1
rrperc <115186086+rrperc@users.noreply.github.com>
2023-02-21 13:04:51 -0800

    FIXEDPART 2

---
+++ Hummana Hummana Awooga/Assets/DUNGEON GEN/SimpleRandomWalk.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalk : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2Int startPosition = Vector2Int.zero;
+
+    [SerializeField]
+    private int iterations = 10;
+    [SerializeField]
+    private int walkLength = 10;
+    [SerializeField]
+    public bool startRandomlyEachiteration = true;
+
+    public void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        foreach (var position in floorPositions)
+        {
+            Debug.Log(position);
+        }
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk()
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+        for (int i = 0; i < iterations; i++)
+        {
+            var path = ProceduralGenerationAlgorithms.SimpleRandomWalk(currentPosition, walkLength);
+            floorPositions.UnionWith(path);
+            if (startRandomlyEachiteration)
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+        }
+        return floorPositions;
+    }
+}
